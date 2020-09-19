de3846e8d4736b8f22fe3791b7b429bb0e712c2f
nymunariya <nicki.aya@web.de>
2020-09-19 11:30:03 +0200

    Create db_radicals.php

---
+++ db_radicals.php
@@ -0,0 +1,100 @@
+<?php
+	
+	class radicalDeck {
+	
+		//protected $cards = [];
+		//protected $groups = [];
+		
+		//187
+		//153
+	
+		public function __construct() {
+			$this->cards = array(
+	   			array (
+			            "id" => "1",
+			            "kanji" => "口",
+			            "forms" => "",
+			            "furigana" => "くち",
+			            "DE" => "",
+			            "EN" => "mouth",
+			            "examples" => "口古可名君否呉告",
+			            "group" => "group1"
+			        ),
+			
+			    array
+			        (
+			            "id" => "2",
+			            "kanji" => "水",
+			            "forms" => "(氵)",
+			            "furigana" => "みず",
+			            "DE" => "",
+			            "EN" => "water",
+			            "examples" => "水永泳決治海演漢瀬",
+			            "group" => "group1"
+			        ),
+			
+			    array
+			        (
+			            "id" => "3",
+			            "kanji" => "木",
+			            "forms" => "",
+			            "furigana" => "き",
+			            "DE" => "",
+			            "EN" => "tree",
+			            "examples" => "木杢板相根森楽機末",
+			            "group" => "group1"
+			        ),
+			
+			    array
+			        (
+			            "id" => "4",
+			            "kanji" => "人",
+			            "forms" => "(亻/𠆢)",
+			            "furigana" => "ひと",
+			            "DE" => "",
+			            "EN" => "person",
+			            "examples" => "仁休位今",
+			            "group" => "group1"
+			        ),
+			
+			    array
+			        (
+			            "id" => "5",
+			            "kanji" => "手",
+			            "forms" => "(扌/龵)",
+			            "furigana" => "て",
+			            "DE" => "Hand",
+			            "EN" => "hand",
+			            "examples" => "手才挙拜拳掌掣擧",
+			            "group" => "group1"
+			        ),
+			
+			    array
+			        (
+			            "id" => "6",
+			            "kanji" => "心",
+			            "forms" => "(忄/⺗)",
+			            "furigana" => "こころ",
+			            "DE" => "Herz",
+			            "EN" => "heart",
+			            "examples" => "必忙忌性悪情想",
+			            "group" => "group1"
+			        )/*,
+			
+			    array
+			        (
+			            "id" => "",
+			            "kanji" => "",
+			            "forms" => "",
+			            "furigana" => "",
+			            "DE" => "",
+			            "EN" => "",
+			            "examples" => "",
+			            "group" => "50%"
+			        )*/
+		    
+		    );
+
+		}
+	}
+?>
